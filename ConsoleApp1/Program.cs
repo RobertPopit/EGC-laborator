b84d89b437ca88fccd87c26dbe78231a19ccbb79
Pamparau Robert <robert.pamparau@student.usv.ro>
2023-10-23 10:30:02 +0300

    Lab3

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            using (Window3D ex = new Window3D())
+            {
+                ex.Run(30.0, 0.0);
+            }
+        }
+    }
+}
