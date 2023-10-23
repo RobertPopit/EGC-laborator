12c4f15c6db729cbe38d4151af83b98bba0f8296
Pamparau Robert <robert.pamparau@student.usv.ro>
2023-10-23 11:39:53 +0300

    Tema laborator 2

--- ConsoleApp1/Window3D.cs
+++ ConsoleApp1/Window3D.cs
@@ -12,6 +12,9 @@ namespace ConsoleApp1
         KeyboardState previousKeyboard;
         Randomizer rando;
         Color DEFAULT_BKG_COLOR = Color.LightBlue;
+        bool showCube = true;
+        KeyboardState lastKeyPress;
+        MouseState prevMouseState;
 
 
         public Window3D() : base(800, 600, new GraphicsMode(32, 24, 0, 8))
@@ -57,31 +60,50 @@ namespace ConsoleApp1
             base.OnUpdateFrame(e);
 
             // COD LOGICA
-            KeyboardState thisKeyboard = Keyboard.GetState();
-            MouseState thisMouse = Mouse.GetState();
+            KeyboardState keyboard = OpenTK.Input.Keyboard.GetState();
+            MouseState mouse = OpenTK.Input.Mouse.GetState();
+            MouseState currentMouseState = Mouse.GetCursorState();
 
-            if (thisKeyboard[Key.Escape])
+            // Se utilizeaza mecanismul de control input oferit de OpenTK (include perifcerice multiple, mai ales pentru gaming - gamepads, joysticks, etc.).
+            if (keyboard[OpenTK.Input.Key.Escape])
             {
                 Exit();
+                return;
             }
-
-            if (thisKeyboard[Key.H] && !previousKeyboard[Key.H])
+            else if (keyboard[OpenTK.Input.Key.A] && !keyboard.Equals(lastKeyPress))
             {
-                displayHelp();
+                // Ascundere comandată, prin apăsarea unei taste - cu verificare de remanență! Timpul de reacțieuman << calculator.
+                if (showCube == true)
+                {
+                    showCube = false;
+                }
+               
             }
-
-            if (thisKeyboard[Key.R] && !previousKeyboard[Key.R])
+            else if (keyboard[OpenTK.Input.Key.P] && !keyboard.Equals(lastKeyPress))
             {
-                GL.ClearColor(DEFAULT_BKG_COLOR);
+                if (showCube == false)
+                {
+                    showCube = true;
+                }
             }
+                lastKeyPress = keyboard;
 
-            if (thisKeyboard[Key.B] && !previousKeyboard[Key.B])
+
+            if (currentMouseState != prevMouseState)
             {
-                GL.ClearColor(rando.GetRandomColor());
+                Console.WriteLine("Mouse X: " + currentMouseState.X + ", Mouse Y: " + currentMouseState.Y);
+
+                if (showCube)
+                {
+                    showCube = false;
+                }
+                else
+                {
+                    showCube = true;
+                }
+
+                prevMouseState = currentMouseState;
             }
-
-
-            previousKeyboard = thisKeyboard;
         }
 
         protected override void OnRenderFrame(FrameEventArgs e)
@@ -90,12 +112,85 @@ namespace ConsoleApp1
 
             GL.Clear(ClearBufferMask.ColorBufferBit);
             GL.Clear(ClearBufferMask.DepthBufferBit);
+            if (showCube == true)
+            {
+                DrawCube();
+                DrawAxes_OLD();
+
+
+
+            }
 
 
             // COD RENDER
 
             SwapBuffers();
 
+        }
+        private void DrawAxes_OLD()
+        {
+            GL.Begin(PrimitiveType.Lines);
+
+            // X
+            GL.Color3(Color.Red);
+            GL.Vertex3(0, 0, 0);
+            GL.Vertex3(20, 0, 0);
+
+            // Y
+            GL.Color3(Color.Blue);
+            GL.Vertex3(0, 0, 0);
+            GL.Vertex3(0, 20, 0);
+
+            // Z
+            GL.Color3(Color.Yellow);
+            GL.Vertex3(0, 0, 0);
+            GL.Vertex3(0, 0, 20);
+
+
+            GL.End();
+        }
+        private void DrawCube()
+        {
+            GL.Begin(PrimitiveType.Quads);
+
+            GL.Color3(Color.Silver);
+            GL.Vertex3(-1.0f, -1.0f, -1.0f);
+            GL.Vertex3(-1.0f, 1.0f, -1.0f);
+            GL.Vertex3(1.0f, 1.0f, -1.0f);
+            GL.Vertex3(1.0f, -1.0f, -1.0f);
+
+            GL.Color3(Color.Honeydew);
+            GL.Vertex3(-1.0f, -1.0f, -1.0f);
+            GL.Vertex3(1.0f, -1.0f, -1.0f);
+            GL.Vertex3(1.0f, -1.0f, 1.0f);
+            GL.Vertex3(-1.0f, -1.0f, 1.0f);
+
+            GL.Color3(Color.Moccasin);
+
+            GL.Vertex3(-1.0f, -1.0f, -1.0f);
+            GL.Vertex3(-1.0f, -1.0f, 1.0f);
+            GL.Vertex3(-1.0f, 1.0f, 1.0f);
+            GL.Vertex3(-1.0f, 1.0f, -1.0f);
+
+            GL.Color3(Color.IndianRed);
+            GL.Vertex3(-1.0f, -1.0f, 1.0f);
+            GL.Vertex3(1.0f, -1.0f, 1.0f);
+            GL.Vertex3(1.0f, 1.0f, 1.0f);
+            GL.Vertex3(-1.0f, 1.0f, 1.0f);
+
+            GL.Color3(Color.PaleVioletRed);
+            GL.Vertex3(-1.0f, 1.0f, -1.0f);
+            GL.Vertex3(-1.0f, 1.0f, 1.0f);
+            GL.Vertex3(1.0f, 1.0f, 1.0f);
+            GL.Vertex3(1.0f, 1.0f, -1.0f);
+
+            GL.Color3(Color.ForestGreen);
+            GL.Vertex3(1.0f, -1.0f, -1.0f);
+            GL.Vertex3(1.0f, 1.0f, -1.0f);
+            GL.Vertex3(1.0f, 1.0f, 1.0f);
+            GL.Vertex3(1.0f, -1.0f, 1.0f);
+
+            GL.End();
         }
 
         private void displayHelp()
